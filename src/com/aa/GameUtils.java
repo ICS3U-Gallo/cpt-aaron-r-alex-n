268e2001d2e821c106ad176237dca955f2fb546f
ARamosCortes <36413085+ARamosCortes@users.noreply.github.com>
2018-06-15 11:00:26 -0400

    Update GameUtils.java

--- src/com/aa/GameUtils.java
+++ src/com/aa/GameUtils.java
@@ -39,19 +39,19 @@ class GameUtils {
         Room entrance = new Room();
         entrance.setName("Entrance");
         entrance.setType("chamber");
-        entrance.setMessage("You enter a poorly lit room with a large hole in the roof, a warm breeze comes over you.\n");
+        entrance.setMessage("You enter a poorly lit room with a large hole in the roof, a warm breeze comes over you.\nYou can go north, east, and west.\n");
 
 
         Room hw1 = new Room();
         hw1.setName("Dark Hallway");
         hw1.setType("corridor");
-        hw1.setMessage("The light around you quickly fades and the pitch darkness fills the hall.\nA bitter cold comes over you, chilling you to the bone.\nA magical message on the wall says in the Dwarven language 'BEWARE OF SENTINEL'.\n");
+        hw1.setMessage("The light around you quickly fades and the pitch darkness fills the hall.\nA bitter cold comes over you, chilling you to the bone.\nA magical message on the wall says in the Dwarven language 'BEWARE OF SENTINEL'.\nYou can go east, or west.\n");
 
 
         Room kr = new Room();
         kr.setName("Knight Room");
         kr.setType("chamber");
-        kr.setMessage("You enter a well lit chamber, statues of past Dwarven kings look down upon you from the walls.\nA horrid stench fills your nose as you realize that the ground is covered in the bones of those who have passed.\n");
+        kr.setMessage("You enter a well lit chamber, statues of past Dwarven kings look down upon you from the walls.\nA horrid stench fills your nose as you realize that the ground is covered in the bones of those who have passed.\nYou can go east or south.\n");
         kr.setKeyId("Glowing Key");
         kr.setBoss(new Enemy("Abyssmal Knight", 20, 10, 45, 20, "Glowing Key"));
 
@@ -59,58 +59,58 @@ class GameUtils {
         Room cr1 = new Room();
         cr1.setName("Hidden Chamber");
         cr1.setType("chamber");
-        cr1.setMessage("You find a small hole in the wall, and walk in, to find yourself facing a rotted chest, years old.\n");
+        cr1.setMessage("You find a small hole in the wall, and walk in, to find yourself facing a rotted chest, years old.\nYou can only go north from here.\n");
 
 
         Room rh = new Room();
         rh.setName("Spiked Hallway");
         rh.setType("corridor");
-        rh.setMessage("You walk into a stone hallway, the floor is old and bloodstained, while the walls are covered in long sharp spikes.\n");
-        rh.setRiddleQuestion("A stone face carved into the wall says\n'In order to proceed, you must prove your worth.\nAnswer me this riddle, in order to leave safely.\nHowever if you displease me, you will meet your death.\n\nAnswer me this: I began eternity, and ended space,\nat the end of time, and in every place,\nlast in life, second to death,\nnever alone, found in your breath,\ncontained by earth, water or flame\nmy grandeur so awesome, wind dare not tame,\nnot in your mind, but in your dreams,\nvacant to kings, present to queens. What am I?");
-        rh.setRiddleQuestion("e");
+        rh.setMessage("You walk into a stone hallway, the floor is old and bloodstained, while the walls are covered in long sharp spikes.\nYou can go east or west.\n");
 
 
         Room d1 = new Room();
         d1.setName("Riddled Door");
         d1.setType("door");
-        d1.setMessage("A door stops your path. There is a carved face on the stonework.\n");
+        d1.setMessage("A door stops your path. There is a carved face in the stonework.\n");
+        rh.setRiddleQuestion("The stone face carved into the wall says\n'In order to proceed, you must prove your worth.\nAnswer me this riddle, in order to leave safely.\nHowever if you displease me, you will meet your death.\n\nAnswer me this: I began eternity, and ended space,\nat the end of time, and in every place,\nlast in life, second to death,\nnever alone, found in your breath,\ncontained by earth, water or flame\nmy grandeur so awesome, wind dare not tame,\nnot in your mind, but in your dreams,\nvacant to kings, present to queens. What am I?");
+        rh.setRiddleQuestion("e");
 
 
         Room cr2 = new Room();
         cr2.setName("Cavern");
         cr2.setType("chamber");
-        cr2.setMessage("The Dwarven architexture rots away as you enter a large carven.\nStalgmites and stalgtites fill the top and bottom of the cave.\nYou feel a slight draft coming from the holes in the walls, however none of them are large enough for you to pass through.\n");
+        cr2.setMessage("The Dwarven architexture rots away as you enter a large carven.\nStalgmites and stalgtites fill the top and bottom of the cave.\nYou feel a slight draft coming from the holes in the walls, however none of them are large enough for you to pass through.\nYou can go north or east.\n");
 
 
         Room hw2 = new Room();
         hw2.setName("Trap-filled Hallway");
         hw2.setType("corridor");
-        hw2.setMessage("As you look down the corridor before you,\nyou notice that there are multiple holes, spikes, and balistas on the walls, roof, and floor.\nThe hallway is booby-trapped and will require careful footing to pass.\n");
+        hw2.setMessage("As you look down the corridor before you,\nyou notice that there are multiple holes, spikes, and balistas on the walls, roof, and floor.\nThe hallway is booby-trapped and will require careful footing to pass.\nYou can go north or south.\n");
 
 
         Room mr1 = new Room();
         mr1.setName("Ruins");
         mr1.setType("chamber");
-        mr1.setMessage("You enter a cave-like room.\nPillars of Dawrven make spike out of the groud, however ubruptly end near an endless cliff.\nThis is mostlikly where the dragon crawled from the speakless depths of the cave system, and entered the dwarven castle...\n");
+        mr1.setMessage("You enter a cave-like room.\nPillars of Dawrven make spike out of the groud, however ubruptly end near an endless cliff.\nThis is mostlikly where the dragon crawled from the speakless depths of the cave system, and entered the dwarven castle...\nYou can go south or west.\n");
 
 
         Room bb = new Room();
         bb.setName("Broken Bridge");
         bb.setType("corridor");
-        bb.setMessage("You enter an underground ravine, a fall from this hight would kill you,\nhowever the fall is so great, there is no way of telling when you would hit the ground.\nLuckily, the ancient dwarves created a stone bridge to cross the ravine,\nbut the years of wear on it has caused it to break in half, the only way to cross would be to jump across.\n");
+        bb.setMessage("You enter an underground ravine, a fall from this hight would kill you,\nhowever the fall is so great, there is no way of telling when you would hit the ground.\nLuckily, the ancient dwarves created a stone bridge to cross the ravine,\nbut the years of wear on it has caused it to break in half, the only way to cross would be to jump across.\nYou can go east or west.\n");
 
 
         Room mr2 = new Room();
         mr2.setName("King's Court");
         mr2.setType("chamber");
-        mr2.setMessage("You enter a large courtroom, dining tables on both sides and a large throne in the centre,\nhowever the ground is covered in slime, and the foul stench makes your stomach turn.\n");
+        mr2.setMessage("You enter a large courtroom, dining tables on both sides and a large throne in the centre,\nhowever the ground is covered in slime, and the foul stench makes your stomach turn.\nYou can go east, south, or west.\n");
         mr2.setBoss(new Enemy("Carnivorous Slime", 30, 15, 35, 20, "Large Iron Key"));
 
 
         Room lh = new Room();
         lh.setName("Looping Hallway");
         lh.setType("corridor");
-        lh.setMessage("You enter a long hallway, with magical runes covering the walls.\nA message on the roof reads 'ONLY THOSE WHO HAVE PROVED THEIR VALUE MAY CONTINUE'.\n");
+        lh.setMessage("You enter a long hallway, with magical runes covering the walls.\nA message on the roof reads 'ONLY THOSE WHO HAVE PROVED THEIR VALUE MAY CONTINUE'.\nYou can go north or south from here.");
 
 
         Room d2 = new Room();
@@ -123,19 +123,19 @@ class GameUtils {
         Room hw3 = new Room();
         hw3.setName("Empty Hallway");
         hw3.setType("corridor");
-        hw3.setMessage("You enter a short hall, with a message written in gold on the walls, multiple times.\nThe ,message reads 'THOSE WHO SHOW AVARICE, WILL DIE THE WAY THEY LIVE.'\n");
+        hw3.setMessage("You enter a short hall, with a message written in gold on the walls, multiple times.\nThe ,message reads 'THOSE WHO SHOW AVARICE, WILL DIE THE WAY THEY LIVE.'\nYou can go east or west from here.\n");
 
 
         Room rt = new Room();
-        rt.setName("Riddled Room");
+        rt.setName("Traped Room");
         rt.setType("chamber");
-        rt.setMessage("You enter a room filled with traps.\nWalking through blindly will be your demise, but with careful footing, you may proceed freely.\n");
+        rt.setMessage("You enter a room filled with traps.\nWalking through blindly will be your demise, but with careful footing, you may proceed freely.\nYou can go east or north.\n");
 
 
         Room cr3 = new Room();
         cr3.setName("Golden Hall");
         cr3.setType("corridor");
-        cr3.setMessage("The hallway before you if filled with golden coins, and artifacts from ancient worlds.\nThe vault that the dragon sleeps in is near.\nThe walls of the room have magic runes inscribed into them,\nand the only thing that is not of value, is a simple wooden chest.\nThe vast gold temps you...\n");
+        cr3.setMessage("The hallway before you if filled with golden coins, and artifacts from ancient worlds.\nThe vault that the dragon sleeps in is near.\nThe walls of the room have magic runes inscribed into them,\nand the only thing that is not of value, is a simple wooden chest.\nThe vast gold temps you...\nYou can go north or south.\n");
 
 
         Room d3 = new Room();
@@ -193,8 +193,8 @@ class GameUtils {
         d2.setSouth(lh);
         lh.setNorth(d2);
 
-        hw3.setEast(rt);
-        rt.setWest(hw3);
+        hw3.setWest(rt);
+        rt.setEast(hw3);
 
         rt.setNorth(cr3);
         cr3.setSouth(rt);
